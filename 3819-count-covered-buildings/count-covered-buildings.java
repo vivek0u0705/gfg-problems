0da2930fe2b796b5c669e40aed0f6c970df3666e
vivekvelugoti <vivekvelugoti070705@gmail.com>
2025-12-11 11:28:06 +0530

    Time: 129 ms (63.16%) | Memory: 278.4 MB (20.00%) - LeetSync

--- 3819-count-covered-buildings/count-covered-buildings.java
+++ 3819-count-covered-buildings/count-covered-buildings.java
@@ -1,20 +1,40 @@
+class Pair{
+    int min;
+    int max;
+    Pair(int m1,int m2){
+        min=m1;
+        max=m2;
+    }
+}
 class Solution {
     public int countCoveredBuildings(int n, int[][] b) {
-        HashMap<Integer,TreeSet<Integer>> xm=new HashMap<>();
-        HashMap<Integer,TreeSet<Integer>> ym=new HashMap<>();
+        HashMap<Integer,Pair> xm=new HashMap<>();
+        HashMap<Integer,Pair> ym=new HashMap<>();
         for(int i=0;i<b.length;i++){
             int u=b[i][0];
             int v=b[i][1];
-            if(!xm.containsKey(u)) xm.put(u,new TreeSet<>());
-            xm.get(u).add(v);
-            if(!ym.containsKey(v)) ym.put(v,new TreeSet<>());
-            ym.get(v).add(u);
+            if(!xm.containsKey(u)){
+                xm.put(u,new Pair(v,v));
+            }
+            else{
+               Pair p=xm.get(u);
+               p.min=Math.min(p.min,v);
+               p.max=Math.max(p.max,v);
+            }
+            if(!ym.containsKey(v)){
+            ym.put(v,new Pair(u,u));
+            }
+            else{
+               Pair p=ym.get(v);
+               p.min=Math.min(p.min,u);
+               p.max=Math.max(p.max,u);
+            }
         }
         int c=0;
         for(int i=0;i<b.length;i++){
             int u=b[i][0];
             int v=b[i][1];
-            if(xm.get(u).first()<v && v<xm.get(u).last() && ym.get(v).first()<u && u<ym.get(v).last()) c++;
+            if(xm.get(u).min<v && v<xm.get(u).max && ym.get(v).min<u && u<ym.get(v).max) c++;
         }
     return c;
     }
