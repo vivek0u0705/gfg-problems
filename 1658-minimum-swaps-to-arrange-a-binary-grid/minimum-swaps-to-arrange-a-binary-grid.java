1cb301068cf4aae02caaa6ad8c42f3707f31ddbb
vivekvelugoti <vivekvelugoti070705@gmail.com>
2026-03-02 11:28:20 +0530

    Time: 2 ms (96.23%) | Memory: 49.5 MB (86.79%) - LeetSync

---
+++ 1658-minimum-swaps-to-arrange-a-binary-grid/minimum-swaps-to-arrange-a-binary-grid.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int minSwaps(int[][] mat) {
+        int n=mat.length;
+        int[] ans=new int[n];
+        for(int i=0;i<n;i++){
+            int t=0;
+            for(int j=n-1;j>=0;j--){
+                if(mat[i][j]==1) {
+                    t=j;
+                    break;
+                }
+            }
+            ans[i]=n-t-1;
+        }
+        int ct=0;
+        for(int i=0;i<n;i++){
+            int nd=n-i-1;
+            int j=i;
+            while(j<n && ans[j]<nd){
+                j++;
+            }
+            if(j==n) return -1;
+            while(j>i){
+                int tm=ans[j];
+                ans[j]=ans[j-1];
+                ans[j-1]=tm;
+                ct++;
+                j--;
+            }
+        }
+    return ct;
+    }
+}
