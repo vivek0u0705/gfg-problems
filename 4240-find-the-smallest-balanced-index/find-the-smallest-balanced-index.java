cfc663b8d3ad17ad28e1f35293beaa37646d3447
vivekvelugoti <vivekvelugoti070705@gmail.com>
2026-03-09 10:42:25 +0530

    Time: 6 ms (66.67%) | Memory: 143.6 MB (100.00%) - LeetSync

--- 4240-find-the-smallest-balanced-index/find-the-smallest-balanced-index.java
+++ 4240-find-the-smallest-balanced-index/find-the-smallest-balanced-index.java
@@ -7,15 +7,18 @@ class Solution {
         arr2[n-1]=1;
         long lt=(long)1e18;
         for(int i=1;i<n;i++){
-            arr1[i]=arr1[i-1]+(long)nums[i-1];
+            arr1[i]=arr1[i-1]+nums[i-1];
         }
+        int idx=0;
         for(int i=n-2;i>=0;i--){
-            if((long)nums[i+1] > lt/arr2[i+1]){
+            if(nums[i+1] > lt/arr2[i+1]){
+                idx=i;
                 arr2[i]=-1;
+                break;
             }
             else arr2[i]=arr2[i+1]*nums[i+1];
         }
-        for(int i=0;i<n;i++){
+        for(int i=idx;i<n;i++){
             if(arr1[i]==arr2[i]){
                 return i;
             }
