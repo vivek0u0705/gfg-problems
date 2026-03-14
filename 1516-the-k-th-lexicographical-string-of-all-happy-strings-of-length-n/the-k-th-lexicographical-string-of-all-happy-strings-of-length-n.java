8372ee589e81b6b80a57dfd41a2c24571e89364a
vivekvelugoti <vivekvelugoti070705@gmail.com>
2026-03-14 12:23:46 +0530

    Time: 13 ms (35.50%) | Memory: 47.6 MB (6.23%) - LeetSync

--- 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.java
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.java
@@ -1,30 +1,23 @@
 class Solution {
     public String getHappyString(int n, int k) {
         List<String> res=new ArrayList<>();
-        StringBuilder sb=new StringBuilder();
-        f(0,n,res,sb);
+        f(0,n,res,"");
         if(k>res.size()) return "";
     return res.get(k-1);
     }
-    public static void f(int i,int n,List<String> res,StringBuilder cur){
+    public static void f(int i,int n,List<String> res,String cur){
         if(i==n){
             res.add(cur.toString());
             return;
         }
         if(cur.length()==0 || cur.charAt(cur.length()-1)!='a'){
-            cur.append('a');
-            f(i+1,n,res,cur);
-            cur.deleteCharAt(cur.length()-1);
+            f(i+1,n,res,cur+'a');
         } 
         if(cur.length()==0 || cur.charAt(cur.length()-1)!='b'){
-            cur.append('b');
-            f(i+1,n,res,cur);
-            cur.deleteCharAt(cur.length()-1);
+            f(i+1,n,res,cur+'b');
         }
         if(cur.length()==0 || cur.charAt(cur.length()-1)!='c'){
-            cur.append('c');
-            f(i+1,n,res,cur);
-            cur.deleteCharAt(cur.length()-1);
+            f(i+1,n,res,cur+'c');
         }
     }
 }
