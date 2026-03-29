b726b535c315021fa12c59aef2311b709dab28de
vivekvelugoti <vivekvelugoti070705@gmail.com>
2026-03-29 13:30:07 +0530

    Time: 206 ms (58.92%) | Memory: 227.6 MB (51.30%) - LeetSync

--- 4267-design-event-manager/design-event-manager.java
+++ 4267-design-event-manager/design-event-manager.java
@@ -11,7 +11,7 @@ class EventManager {
         if(a.p!=b.p) return Integer.compare(b.p,a.p);
         else return Integer.compare(a.v,b.v);
     });
-    HashMap<Integer,Integer> hm=new HashMap<>();
+    HashMap<Integer,Integer> hm=new HashMap<>(); //intailize to get val in pq
     public EventManager(int[][] events) {
         int n=events.length;
         for(int i=0;i<n;i++){
@@ -23,14 +23,16 @@ class EventManager {
     
     public void updatePriority(int eventId, int newPriority) {
         hm.put(eventId,newPriority);
-        pq.add(new Pair(eventId,newPriority));
+        pq.add(new Pair(eventId,newPriority)); // add val even priority is low or high
 
     }
     
     public int pollHighest() {
         // if(pq.size()==0) return -1;
         while(!pq.isEmpty()){
-            Pair rem=pq.poll();
+            Pair rem=pq.poll(); // remove all top in pq
+    //if top prio is there in hm then is valid then remove in hm for next testcases
+    //return it val
             if(hm.containsKey(rem.v) && hm.get(rem.v)==rem.p){
                 hm.remove(rem.v);
                 return rem.v;
