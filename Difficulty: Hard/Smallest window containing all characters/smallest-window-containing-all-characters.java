517ec91077e6450b815a17966480b124645f6bbe
vivekvelugoti <vivekvelugoti070705@gmail.com>
2026-04-28 14:32:45 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Smallest window containing all characters/smallest-window-containing-all-characters.java
+++ Difficulty: Hard/Smallest window containing all characters/smallest-window-containing-all-characters.java
@@ -2,27 +2,31 @@ class Solution {
     public static String minWindow(String s, String t) {
         // code here
         int n=s.length();
-        int ml=(int)1e9;
         int si=-1;
-        int[] ch=new int[256];
-        for(int j=0;j<t.length();j++) ch[t.charAt(j)]++;
-        int ct=0;
+        int[] ch=new int[26];
+        int[] chh=new int[26];
+        for(int j=0;j<t.length();j++) ch[t.charAt(j)-'a']++;
         int l=0;
+        int minL=(int)1e9;
         for(int r=0;r<n;r++){
             char c=s.charAt(r);
-            if(ch[c]>0) ct++;
-            ch[c]--;
-            while(l<=r && ct==t.length()){
-                if(r-l+1<ml){
-                    ml=r-l+1;
+            chh[c-'a']++;
+            while(contains(chh,ch)){
+                if(r-l+1<minL){
+                    minL=r-l+1;
                     si=l;
                 }
-                ch[s.charAt(l)]++;
-                if(ch[s.charAt(l)]>0) ct=ct-1;
+                chh[s.charAt(l)-'a']--;
                 l++;
             }
         }
         if(si==-1) return "";
-    return s.substring(si,si+ml);
+    return s.substring(si,si+minL);
+    }
+    public static boolean contains(int[] chh,int ch[]){
+        for(int i=0;i<26;i++){
+            if(chh[i]<ch[i]) return false;
+        }
+    return true;
     }
 }
