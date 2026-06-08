c7bb9bb151ff2f4849b138fcd1cef83dfb5b01a4
vivekvelugoti <vivekvelugoti070705@gmail.com>
2026-06-08 21:58:00 +0530

    Time: 24 ms (56.33%) | Memory: 46.7 MB (13.41%) - LeetSync

---
+++ 698-partition-to-k-equal-sum-subsets/partition-to-k-equal-sum-subsets.java
@@ -0,0 +1,40 @@
+class Solution {
+    public boolean canPartitionKSubsets(int[] nums, int k) {
+        int ts=0;
+        int n=nums.length;
+        for(int i:nums) ts+=i;
+        if(ts%k!=0) return false;
+        int tar=ts/k;
+        int mask=(1<<n)-1;
+        int[] dp=new int[mask+1];
+        Arrays.fill(dp,-1);
+    return f(nums,tar,mask,0,dp);
+    }
+    static boolean f(int[] nums,int tar,int mask,int curSum,int[] dp){
+        int n=nums.length;
+        if(mask==0) {
+            if(curSum%tar==0) return true;
+            else return false;
+        }
+        if(dp[mask]!=-1) return dp[mask]==1;
+        if(curSum==tar) return f(nums,tar,mask,0,dp);
+        for(int i=0;i<n;i++){
+            if((mask&(1<<i))==0) continue;
+            if(curSum+nums[i]> tar) continue;
+            int Nmask=(mask & ~(1<<i));
+            if(f(nums,tar,Nmask,curSum+nums[i],dp)){
+                dp[mask]=1;
+                return true;
+            }
+        }
+    dp[mask]=0;
+    return false;
+    }
+}
+
+
+//bitmask dp 
+
+// we keep (0 -> n-1) all set bits 1's for to check is present in subset or not 
+
+// we check 0 to 2 ^n -1 at last we are in all unset bits then we print result;
