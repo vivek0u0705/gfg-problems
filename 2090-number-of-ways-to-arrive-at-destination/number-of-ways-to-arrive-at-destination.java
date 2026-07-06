20cf4dac65237b883b594189b7aa55f6733aee4a
vivekvelugoti <vivekvelugoti070705@gmail.com>
2026-07-06 17:11:29 +0530

    Time: 15 ms (26.18%) | Memory: 52 MB (51.38%) - LeetSync

--- 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.java
+++ 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.java
@@ -11,44 +11,41 @@ class Solution {
     public int countPaths(int n, int[][] roads) {
         //dijktras + some logic with ways array
         List<List<int[]>> adj=new ArrayList<>();
-        long[] dist=new long[n];
-        int[] ways=new int[n];
-        Arrays.fill(dist,Long.MAX_VALUE);
-        dist[0]=0;
-        ways[0]=1;
-
-        for(int i=0;i<n;i++){
-            adj.add(new ArrayList<>());
-        }
+        for(int i=0;i<n;i++) adj.add(new ArrayList<>());
         for(int i=0;i<roads.length;i++){
             int u=roads[i][0];
             int v=roads[i][1];
             int w=roads[i][2];
-            adj.get(u).add(new int[]{v,w});
-            adj.get(v).add(new int[]{u,w});
+            adj.get(u).add(new int[]{v,w}); 
+            adj.get(v).add(new int[]{u,w}); 
         }
+        long[] dist=new long[n];
+        int[] ways=new int[n];
+        Arrays.fill(dist,(long)1e18);
+        dist[0]=0;
+        ways[0]=1;
         PriorityQueue<Pair> pq=new PriorityQueue<>((a,b)->Long.compare(a.wt,b.wt));
-        pq.add(new Pair(0,0)); // v,wt
+        pq.add(new Pair(0,0));
 
         while(!pq.isEmpty()){
-            int node=pq.peek().nd;
-            long wtt=pq.peek().wt;
-            pq.poll();
+            Pair cur = pq.poll();
+            int u = cur.nd;
+            long d = cur.wt;
 
-            for(int[] adjN : adj.get(node)){
-                int edN=adjN[0];
-                int edW=adjN[1];
-                if(wtt+edW < dist[edN]){
-                    dist[edN]=wtt+edW;
-                    pq.add(new Pair(edN,dist[edN]));
-                    ways[edN]=ways[node];
+            for(int[] adjN :adj.get(u)){
+                int v=adjN[0];
+                int wtt=adjN[1];
+                if(d+wtt<dist[v]){
+                    dist[v]=d+wtt;
+                    pq.add(new Pair(v,dist[v]));
+                    ways[v]=ways[u];
                 }
-                else if(wtt+edW == dist[edN]){
-                    ways[edN] = (ways[edN] + ways[node]) % mod;
+                else if(d+wtt==dist[v]){
+                    ways[v]=(ways[v]+ways[u])%mod;
                 }
             }
         }
-    return ways[n-1] % mod;
+    return ways[n-1];
     }
 }
 
