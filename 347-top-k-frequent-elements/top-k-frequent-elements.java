1e0fda7eb02832873f826be848abfe8fd7bd49d9
vivekvelugoti <vivekvelugoti070705@gmail.com>
2026-07-19 12:19:51 +0530

    Time: 14 ms (70.90%) | Memory: 47.5 MB (82.02%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -1,13 +1,27 @@
+class Pair{
+    int k;
+    int v;
+    Pair(int key,int val){
+        v=val;
+        k=key;
+    }
+}
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
         int n=nums.length;
         HashMap<Integer,Integer> hm=new HashMap<>();
         for(int i=0;i<n;i++) hm.put(nums[i],hm.getOrDefault(nums[i],0)+1);
-        List<Integer> li=new ArrayList<>(hm.keySet());
-        li.sort((a,b)->Integer.compare(hm.get(b),hm.get(a)));
-        int[] arr=new int[k];
-        for(int i=0;i<k;i++){
-            arr[i]=li.get(i);
+        PriorityQueue<Pair> pq=new PriorityQueue<>((a,b)->Integer.compare(a.v,b.v));
+        for(int key :hm.keySet()){
+            // System.out.println(key +" "+hm.get(key));
+            pq.add(new Pair(key,hm.get(key)));
+            if(pq.size()>k) pq.poll();
+        }
+        int arr[]=new int[k];
+        int i=0;
+        while(!pq.isEmpty()){
+            arr[i++]=pq.peek().k;
+            pq.poll();
         }
     return arr;
     }
