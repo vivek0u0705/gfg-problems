d01e3edae05b1a70a4726ea242022402d4f0a649
vivekvelugoti <vivekvelugoti070705@gmail.com>
2026-07-25 20:47:47 +0530

    Time: 3 ms (14.93%) | Memory: 44.8 MB (44.76%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.java
+++ 22-generate-parentheses/generate-parentheses.java
@@ -3,11 +3,10 @@ class Solution {
         List<String> r=new ArrayList<>();
         g(n,r,"",0,0);
     return r;
-
     }
     public static void g(int n,List<String> r,String cur,int o,int c){
         if(c>o) return;
-        if(cur.length()==2*n) {
+        if(o+c==2*n && c==o) {
             r.add(cur);
             return;
         }
