387533cf807cc9dd490d6fc1bc2b8e2be59534b1
vivekvelugoti <vivekvelugoti070705@gmail.com>
2026-07-28 16:17:12 +0530

    Time: 98 ms (42.00%) | Memory: 254.4 MB (20.29%) - LeetSync

---
+++ 1300-critical-connections-in-a-network/critical-connections-in-a-network.java
@@ -0,0 +1,49 @@
+class Solution {
+    int timer=1;
+    int[] dt;
+    int[] low;
+    List<List<Integer>> adj;
+    List<List<Integer>> res;
+    public List<List<Integer>> criticalConnections(int n, List<List<Integer>> edges) {
+        adj=new ArrayList<>();
+        for(int i=0;i<n;i++){
+            adj.add(new ArrayList<>());
+        }
+        for(int i=0;i<edges.size();i++){
+            int u=edges.get(i).get(0);
+            int v=edges.get(i).get(1);
+            adj.get(u).add(v);
+            adj.get(v).add(u);
+        }
+        int par=-1;
+        int vis[]=new int[n];
+        dt=new int[n];
+        low=new int[n];
+        res=new ArrayList<>();
+        dfs(0,par,vis);
+    return res;
+    }
+    public void dfs(int u,int par,int[] vis){
+        vis[u]=1;
+        dt[u]=low[u]=timer++;
+        for(int v : adj.get(u)){
+            if(vis[v]==0){
+                dfs(v,u,vis);
+                low[u]=Math.min(low[u],low[v]);
+                if(low[v]>dt[u]) {
+                    res.add(Arrays.asList(u,v));
+                }
+            }
+            else if(v!=par){
+                low[u]=Math.min(low[u],low[v]);
+            }
+        }
+    }
+}
+
+
+
+//tarjans algorithm   uses dt,low  to track bridge
+//if vis adj not parent then make low[u] min of all its adjcents low
+//if not vis make dfs and make low[u] min 
+//low[v] > dt[u] → Child cannot reach the parent or any ancestor except through this edge
