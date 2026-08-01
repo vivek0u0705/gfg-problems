d4d22f2ed412a7cad41e02e49ef0102eb55fe12b
vivekvelugoti <vivekvelugoti070705@gmail.com>
2026-08-01 23:21:02 +0530

    Time: 86 ms (11.74%) | Memory: 42.8 MB (57.54%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.java
+++ 486-predict-the-winner/predict-the-winner.java
@@ -10,8 +10,8 @@ class Solution {
     }
     static int f(int i,int j,int[] arr){
         if(i>j) return 0;
-        if(i==j) return arr[i];
-        if(i==j+1) return Math.max(arr[i],arr[j]);
+        // if(i==j) return arr[i];
+        // if(i==j+1) return Math.max(arr[i],arr[j]);
         int start=arr[i]+Math.min(f(i+2,j,arr),f(i+1,j-1,arr));
         int end=arr[j]+Math.min(f(i,j-2,arr),f(i+1,j-1,arr));
     return Math.max(start,end);
