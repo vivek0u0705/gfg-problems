c6aec66ff85400101b06c6d55e99b18a6c230cc2
vivekvelugoti <vivekvelugoti070705@gmail.com>
2026-08-01 23:20:43 +0530

    Time: 63 ms (29.32%) | Memory: 42.6 MB (74.96%) - LeetSync

---
+++ 486-predict-the-winner/predict-the-winner.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean predictTheWinner(int[] nums) {
+        //removal game  find the player 1 score if both plays optimally
+        //interval dp (start i=0 && j=n-1)
+        int n=nums.length;
+        int res=f(0,n-1,nums);
+        long sum=0;
+        for(int x:nums)sum+=x;
+    return (sum-res)<=res;
+    }
+    static int f(int i,int j,int[] arr){
+        if(i>j) return 0;
+        if(i==j) return arr[i];
+        if(i==j+1) return Math.max(arr[i],arr[j]);
+        int start=arr[i]+Math.min(f(i+2,j,arr),f(i+1,j-1,arr));
+        int end=arr[j]+Math.min(f(i,j-2,arr),f(i+1,j-1,arr));
+    return Math.max(start,end);
+    }
+}
